cde0f96f8723df9d6ff92752ce3eee11932e58ef
rustam ganiev <psy12@yandex.ru>
2025-10-03 20:22:50 +0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ganiev-dev/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ganiev-dev/java-project-78/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=ganiev-dev_java-project-78&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=ganiev-dev_java-project-78)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=ganiev-dev_java-project-78&metric=coverage)](https://sonarcloud.io/summary/new_code?id=ganiev-dev_java-project-78)
+ # Валидатор данных 
---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.3.1.5724"
+    id("jacoco")
+    checkstyle
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ganiev-dev_java-project-78")
+        property("sonar.organization", "ganiev-dev")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.junit.reportPaths", "build/test-results/test")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+
+    }
+}
+
+dependencies {
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.3")  // API для тестов
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.3") // Движок для запуска
+    testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+    implementation ("info.picocli:picocli:4.7.7")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.7")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.0")
+    testImplementation("org.assertj:assertj-core:3.26.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+jacoco {
+    toolVersion = "0.8.13"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml"))
+        html.required.set(true)
+    }
+}
